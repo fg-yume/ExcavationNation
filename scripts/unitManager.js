bb3ae194fbfcbda78770a221c97add9f482c177f
Freddy Garcia <garciaf93@gmail.com>
2016-01-31 14:26:17 -0500

    Adds new version of unit and unit manager. Also includes them in index file. Still need to incorporate into game

--- scripts/unitManager.js
+++ scripts/unitManager.js
@@ -1,75 +1,18 @@
 "use strict";
 
 // namespace
-var PhaserGame = PhaserGame || {};
-var Engine = Engine || {};
+var UnitManager = {};
 
-Engine.UnitTypes = {
-    MELEE  : 0,
-    RANGED : 1,
-    NUM_TYPES : 2,
-}
+UnitManager.Units = function(game){
+    Phaser.Group.call(this, game, game.world, 'Units', false, true, Phaser.Physics.ARCADE);
 
-Engine.UnitManager = (function()
+    return this;
+};
+
+UnitManager.Units.prototype = Object.create( Phaser.Group.prototype );
+UnitManager.Units.prototype.constructor = UnitManager.Units;
+
+UnitManager.Units.prototype.createUnit = function( game, x, y, hp, imgKey )
 {
-    var UnitManager = function()
-    {
-        // unit group
-        this.units = game.add.group();
-        this.units.enableBody = true;
-        this.units.physicsBodyType = Phaser.Physics.ARCADE;
-
-        // keeps track of unit types that have been destroyed
-        this.destroyedUnits = new Map(); // UnitType / amount
-    };
-
-    UnitManager.prototype.reset = function()
-    {
-        this.destroyedUnits = new Map();
-        this.units.destroy( true );
-    }
-
-    UnitManager.prototype.createUnit = function(x, y, unitType, hp, assetName)
-    {
-        var unit = this.units.create(x, y, assetName);
-        unit.health = hp;
-        unit.prototype.unitType = (unitType === "undefined") ? Engine.UnitTypes.NUM_TYPES : unitType;
-
-        // TODO: phaser anchors and animations
-    }
-
-    // Only handles adding to list of destroyed units
-    UnitManager.prototype.destroyUnit = function(unitType)
-    {
-        // If doesn't exist
-        var amnt = this.destroyedUnits.get( unitType );
-        if(  amnt === "undefined" )
-        {
-            this.destroyedUnits.set( unitType, 1 );
-            return;
-        }
-
-        // Increment amount that have been destroyed
-        this.destroyedUnits.set( unitType, amnt+1 );
-    }
-
-    UnitManager.prototype.amountDestroyed = function( unitType )
-    {
-        var amnt = this.destroyedUnits.get( unitType );
-
-        if( amnt === "undefined" )
-        { 
-            return 0; 
-        }
-        else
-        { 
-            return amnt; 
-        }
-    }
-
-    UnitManager.prototype.update = function()
-    {
-
-    }
-
-})();
+    this.add( new Unit( game, x, y, hp, imgKey ));
+}
